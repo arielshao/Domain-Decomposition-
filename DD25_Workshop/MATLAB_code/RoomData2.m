ee87396c7fec32970cb2489c96c9787482c61707
AILI <aili.shao@math.ubc.ca>
2018-08-28 00:49:30 +0800

    add data for floor heater

---
+++ DD25_Workshop/MATLAB_code/RoomData2.m
@@ -0,0 +1,11 @@
+eta=0; J=20;                                 % no of interior mesh points
+h=1/(J+1);                                   % mesh size 
+x=(0:h:1); y=x;                              % mesh in x and y direction
+f=zeros(J,J);                                % interior source function
+xi=x(2:end-1); yi=xi;                        % interior mesh in x-y direction 
+% f([yi>0.4 & yi<0.6],[xi>0.4 & xi<0.6])=50;   % heater placement
+f([yi>0.1 & yi<0.9],[xi>0.1 & xi<0.9])=8;   % floor heater
+gg=0.3*ones(J,1); gg(yi>0.5 & yi<0.9)=1;     % door right hand side
+gd=zeros(J,1);
+
+
