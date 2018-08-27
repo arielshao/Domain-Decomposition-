393c9515803abdc9884df47714de950ed4896b2f
Aili Shao <31915817+arielshao@users.noreply.github.com>
2018-08-28 00:11:43 +0800

    Delete A1d.m

--- DD25 Workshop/MATLAB code/A1d.m
+++
@@ -1,8 +0,0 @@
-function A=A1d(eta,a,b,J)
-% A1D one dimensional finite difference approximation
-%   A=A1d(eta,a,b,J) computes a sparse finite difference approximation
-%   of the one dimensional operator eta-Delta on the domain
-%   Omega=(a,b) using J interior points
-
-h=(b-a)/(J+1); e=ones(J,1);
-A=spdiags([-e/h^2 (eta+2/h^2)*e -e/h^2],[-1 0 1],J,J);
